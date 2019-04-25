b695a74f7377428f282857089acb3fd2c0e30481
d-lean001 <david.e.lean@gmail.com>
2019-04-24 18:32:27 -0700

    Added button presses in myScript.js
    added id's for the buttons to react to events of onclick

---
+++ myScript.js
@@ -0,0 +1,19 @@
+function upButtonPress() {
+  console.log("Up is pressed");
+  document.getElementById('debugText').innerHTML = "Up is pressed";
+}
+
+function downButtonPress() {
+  console.log("Down is pressed");
+  document.getElementById('debugText').innerHTML = "Down is pressed";
+}
+
+function leftButtonPress() {
+  console.log("Left is pressed");
+  document.getElementById('debugText').innerHTML = "Left is pressed";
+}
+
+function rightButtonPress() {
+  console.log("Right is pressed");
+  document.getElementById('debugText').innerHTML = "Right is pressed";
+}
